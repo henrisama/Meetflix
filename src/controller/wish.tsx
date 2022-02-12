afd0e285a7c7fc4107132123c1ad8eaecf13ba8a
henrisama <pedropiresflores@icloud.com>
2022-02-12 11:18:53 -0300

    CRUD wish list (backend)

--- src/controller/wish.tsx
+++ src/controller/wish.tsx
@@ -1,11 +1,196 @@
-export const getWish = (): void => {
+import { NextApiRequest, NextApiResponse } from 'next';
+import User from '../models/users.models';
 
+export const getWish = async (
+	req: NextApiRequest, 
+	res: NextApiResponse
+): Promise<void> => {
+	const { id_user, id_profile } = req.cookies;
+
+	if(id_user && id_profile){
+		await User.findById({ _id: id_user })
+			.then(function (user){
+				if(!user){
+					res
+						.status(404)
+						.json({ 
+							success: false,
+							err: 'User not found'
+						});
+					return;
+				}
+				
+				const response = user
+					.profiles[Number(id_profile)]
+					.list
+					.wish;
+
+				res
+					.status(200)
+					.json({
+						success: true,
+						data: response
+					});
+				return;
+			})
+			.catch(function (err){
+				res
+					.status(502)
+					.json({ 
+						success: false,
+						err: 'Error getting wishlist in mongodb'
+					});
+				return;
+			});
+	}else{
+		res
+			.status(400)
+			.json({
+				success: false,
+				err: 'Missing data'
+			});
+		return;
+	}
 };
 
-export const addWish = (id: number): void => {
+export const addWish = async (
+	id: number,
+	req: NextApiRequest, 
+	res: NextApiResponse
+): Promise<void> => {
+	const { id_user, id_profile } = req.cookies;
 
-};
+	if(id_user	&& id_profile && id){
+		const user = await User.findById({ _id: id_user })
+			.then(function(user) {
+				if(!user){
+					res
+						.status(404)
+						.json({ 
+							success: false,
+							err: 'User not found'
+						});
+					return;
+				}
+				return user;
+			})
+			.catch(() => {
+				res
+					.status(502)
+					.json({ 
+						success: false,
+						err: 'Error getting wishlist in mongodb'
+					});
+				return;
+			});
 
-export const delWish = (id: number): void => {
+		try{
+			const index = (user.profiles[id_profile].list.wish as Array<number>)
+				.findIndex(element => element === id);
+
+			if(index === -1){
+				(user.profiles[Number(id_profile)].list.wish as Array<number>).push(id);
+			}
   
+			await User.findByIdAndUpdate({ 
+				_id: id_user
+			}, {
+				profiles: user.profiles 
+			});
+		}catch(err){
+			res
+				.status(502)
+				.json({ 
+					success: false,
+					err: 'Could not add movie/tv'
+				});
+			return;
+		}
+  
+		res
+			.status(200)
+			.json({
+				success: true,
+				message: 'Successfuly added'
+			});
+		return;
+	}else{
+		res
+			.status(400)
+			.json({
+				success: false,
+				err: 'Missing data'
+			});
+		return;
+	}
+};
+
+export const delWish = async (
+	id: number,
+	req: NextApiRequest, 
+	res: NextApiResponse
+): Promise<void> => {
+	const { id_user, id_profile } = req.cookies;
+
+	if(id_user	&& (id_profile != undefined) && id){
+		const user = await User.findById({ _id: id_user })
+			.then(function(user) {
+				if(!user){
+					res
+						.status(404)
+						.json({ 
+							success: false,
+							err: 'User not found'
+						});
+					return;
+				}
+				return user;
+			})
+			.catch(() => {
+				res
+					.status(502)
+					.json({ 
+						success: false,
+						err: 'Error getting wishlist in mongodb'
+					});
+				return;
+			});
+
+		try{
+			const index = (user.profiles[id_profile].list.wish as Array<number>)
+				.findIndex(element => element === id);
+
+			if(index >= 0){
+				(user.profiles[id_profile].list.wish as Array<number>).splice(index, 1);
+			}
+
+			await User.findByIdAndUpdate({ 
+				_id: id_user
+			}, {
+				profiles: user.profiles 
+			});
+		}catch(err){
+			res
+				.status(502)
+				.json({ 
+					success: false,
+					err: 'Could not remove movie/tv'
+				});
+			return;
+		}
+  
+		res
+			.status(200)
+			.json({
+				success: true,
+				message: 'Successfuly deleted'
+			});
+	}else{
+		res
+			.status(400)
+			.json({
+				success: false,
+				err: 'Missing data'
+			});
+	}
 };
--- src/pages/api/user/profile/wish.tsx
+++ src/pages/api/user/profile/wish.tsx
@@ -1,13 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import isAuthaticated from '@/src/middlewares/isAuthanticated';
+import { addWish, delWish, getWish } from '@/src/controller/wish';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
 
 	switch (req.method) {
 	case 'GET':
-			
+		getWish(req,res);
 		break;
-	
+	case 'POST':
+		addWish(req.body.id, req, res);
+		break;
+	case 'DELETE':
+		delWish(req.body.id, req, res);
+		break;
 	default:
 		return res.status(405).send({ message: `Method ${req.method} not allowed` });
 	}
